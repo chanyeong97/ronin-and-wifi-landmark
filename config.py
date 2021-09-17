e819d430e3a376d16d13ee354ce9250f450f4c6c
cksdi <cksdi939767@gmail.com>
2021-09-17 19:07:58 +0900

    first commit

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ MS2S = 1e6
 
 TRAIN_DATASET_PATH = os.path.join('dataset', 'train')
 
-RONIN_RESNET_MODEL_PATH = os.path.join('models', 'ronin_resnet', 'checkpoint_gsn_latest.pt')
-AUTOENCODER_MODEL_PATH = os.path.join('models', 'autoencoder', 'checkpoint')
+RONIN_RESNET_MODEL_PATH = os.path.join('trained_models', 'ronin_resnet', 'checkpoint_gsn_latest.pt')
+AUTOENCODER_MODEL_PATH = os.path.join('trained_models', 'autoencoder', 'checkpoint')
 
 def get_train_dataset():
     return glob(os.path.join(TRAIN_DATASET_PATH, '*'))
